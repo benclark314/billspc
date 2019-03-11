5b8afaf184c9d10affc1215bab3581a1b76c3c0b
Ben Clark <benclark314@hotmail.com>
2019-03-11 10:31:14 -0500

    Removed Trainer Pokemon Controller routes (not using them anymore). Made interface for CSV upload on the index page.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,6 +10,14 @@
     <hr class="my-4">
     <p>When you change a Pokemon box, data will be saved. Is that okay?</p>
     <!-- <p><a class="btn btn-primary btn-lg" href="/login" role="button">Log in</a> <a class="btn btn-success btn-lg" href="/register" role="button">Sign up</a> -->
+    <form action="{{url('/pokemon')}}" method="post" enctype="multipart/form-data">
+    {{csrf_field()}}
+       <div class="form-group">
+           <label for="upload-file">Upload CSV</label>
+           <div class="container"><input type="file" name="upload-file" class="form-control"></div>
+       </div>
+       <input class="btn btn-success" type="submit" value="Upload File" name="submit">
+    </form>
   </div>
 </div>
 @endsection
